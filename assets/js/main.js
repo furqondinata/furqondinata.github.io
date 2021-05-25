df9f69dcc266fad6643a74f34b2f51d64f5ff0bf
furqondinata <fdonata123@gmail.com>
2021-05-25 20:23:05 +0700

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -189,3 +189,30 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+// text berjalan
+const txtElement = ['#Open To Work', 'Web Developer',];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+	if(count == txtElement.length){
+		count = 0;
+	}
+
+	currentTxt = txtElement[count];
+
+	words = currentTxt.slice(0, ++txtIndex);
+	document.querySelector('.efek-ngetik').textContent = words;
+
+	if(words.length == currentTxt.length){
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(ngetik, 130);
+
+})();
